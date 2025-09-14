17fe874f850db419c19f1362f7ba85eaec10c1d6
S V Preethi <162338374+Preton24@users.noreply.github.com>
2025-09-14 11:55:51 +0530

    final changes

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import easyocr
 import cv2
 from matplotlib import pyplot as plt
-import numpy as np
 
-IMAGE_PATH= "smile.jpg"
+IMAGE_PATH = 'DangerSign.jpg'
+
 reader = easyocr.Reader(['en'])
-result = reader.readtext(IMAGE_PATH)
-print(result)
-
-top_left = tuple(result[0][0][0])
-bottom_right = tuple(result[0][0][2])
-text = result[0][1]
-font = cv2.FONT_HERSHEY_SIMPLEX
+results = reader.readtext(IMAGE_PATH)
 img = cv2.imread(IMAGE_PATH)
-img = cv2.rectangle(img,top_left,bottom_right,(0,255,0),3)
-img = cv2.putText(img,text,top_left, font, 0.5,(255,255,255),2,cv2.LINE_AA)
-plt.imshow(img)
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+for detection in results:
+    coords, text, confidence = detection
+    top_left = tuple(map(int, coords[0]))
+    bottom_right = tuple(map(int, coords[2]))
+    img = cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2) 
+    img = cv2.putText(img, text, (top_left[0], top_left[1] - 10), font, 
+                      0.7, (255, 0, 0), 2, cv2.LINE_AA)
+
+plt.figure(figsize=(10,10))
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.axis("off")
 plt.show()
